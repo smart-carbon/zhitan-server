e3b39ab8f2bc13e303d03248978083df1c798ca9
Mr1wei <136831343+Mr1wei@users.noreply.github.com>
2024-01-25 22:23:55 +0800

    修正

--- energy_management_ui/src/api/reportInfo/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function govReportList(query) {
-  return request({
-    url: '/govReports/govReports/list',
-    method: 'get',
-    params: query
-  })
-}
